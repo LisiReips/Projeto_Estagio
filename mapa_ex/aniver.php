5b301b2f3f6f72e81bc3358e29b7ed1d00d2c10e
usuario <gian.vieira@IBI-1001-29197.COTRIBA.INTERNO>
2018-03-01 17:47:30 -0300

    Alteração em aniver.php

--- mapa_ex/aniver.php
+++ mapa_ex/aniver.php
@@ -1,60 +1,71 @@
 <?php
-require '../mainfile.php';
-require '../classes/PHPExcel-1.8/Classes/PHPExcel.php';
+  require '../mainfile.php';
+  require '../classes/PHPExcel-1.8/Classes/PHPExcel.php';
 
-if (isset($_POST['pesquisa'])) {
-  error_reporting(E_ALL);
-  ini_set('display_errors', TRUE);
-  ini_set('display_startup_errors', TRUE);
-  date_default_timezone_set('America/Sao Paulo');
+  if (isset($_POST['pesquisa'])) {
+    error_reporting(E_ALL);
+    ini_set('display_errors', TRUE);
+    ini_set('display_startup_errors', TRUE);
+    setlocale(LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese');
+    date_default_timezone_set('America/Sao_Paulo');
 
-  $conexao = new PgConn();
+    $conexao = new PgConn();
 
-  $inicial = $_POST['inicial'];
-  $final = $_POST['final'];
+    $inicial = $_POST['inicial'];
+    $final = $_POST['final'];
 
-  $pesquisa = "select p.nome, COALESCE(p.email,'SEM EMAIL') as email, 
+    $pesquisa = "select p.nome, COALESCE(p.email,'SEM EMAIL') as email, 
   COALESCE(p.telefone,'SEM TELEFONE') as telefone, to_char(p.idade,'dd/mm/yyyy') as data
   from pacientes p
   where p.idade between '" . $inicial . "' and '" . $final . "'";
 
-  $resultados = $conexao->executar($pesquisa);
+    $resultados = $conexao->executar($pesquisa);
 
-  $objPHPExcel = new PHPExcel();
-  $objPHPExcel->getProperties()->setCreator("Clínica CIEMP")
-          ->setTitle("Aniversariantes")
-          ->setSubject("Aniversariantes");
+    $objPHPExcel = new PHPExcel();
+    $objPHPExcel->getProperties()->setCreator("Clínica CIEMP")
+      ->setTitle("Aniversariantes")
+      ->setSubject("Aniversariantes");
 
-  $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'NOME');
-  $objPHPExcel->getActiveSheet()->SetCellValue('B1', 'DATA');
-  $objPHPExcel->getActiveSheet()->SetCellValue('C1', 'TELEFONE');
-  $objPHPExcel->getActiveSheet()->SetCellValue('D1', 'EMAIL');
+    $objPHPExcel->getActiveSheet()->SetCellValue('A1', 'NOME');
+    $objPHPExcel->getActiveSheet()->SetCellValue('B1', 'DATA');
+    $objPHPExcel->getActiveSheet()->SetCellValue('C1', 'TELEFONE');
+    $objPHPExcel->getActiveSheet()->SetCellValue('D1', 'EMAIL');
 
-  $n_linhas = 2;
-  foreach ($resultados as $resultado) {
-    $objPHPExcel->getActiveSheet()->SetCellValue('A' . $n_linhas, $resultado['nome']);
-    $objPHPExcel->getActiveSheet()->SetCellValue('B' . $n_linhas, $resultado['data']);
-    $objPHPExcel->getActiveSheet()->SetCellValue('C' . $n_linhas, $resultado['telefone']);
-    $objPHPExcel->getActiveSheet()->SetCellValue('D' . $n_linhas, $resultado['email']);
-    $n_linhas++;
+    $objPHPExcel->getActiveSheet()->getStyle("A1:CX1")->getFill()
+      ->applyFromArray(array("type" => PHPExcel_Style_Fill::FILL_SOLID, "startcolor" => array("rgb" => "D8E4BC")));
+    
+    $n_linhas = 2;
+    foreach ($resultados as $resultado) {
+      $objPHPExcel->getActiveSheet()->SetCellValue('A' . $n_linhas, $resultado['nome']);
+      $objPHPExcel->getActiveSheet()->SetCellValue('B' . $n_linhas, PHPExcel_Shared_Date::PHPToExcel($resultado['data']));
+      $objPHPExcel->getActiveSheet()->SetCellValue('C' . $n_linhas, $resultado['telefone']);
+      $objPHPExcel->getActiveSheet()->SetCellValue('D' . $n_linhas, $resultado['email']);
+      $objPHPExcel->getActiveSheet()->getStyle('B' . $n_linhas)->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_DATE_DDMMYYYY);
+      $n_linhas++;
+    }
+
+    $objPHPExcel->setActiveSheetIndex(0);
+    $objPHPExcel->getActiveSheet()->setTitle('ANIVERSARIANTES');
+
+
+    $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+    $objWriter->save('aniversariantes.xlsx');
+
+    if (file_exists('aniversariantes.xlsx')) {
+      $finfo = finfo_open(FILEINFO_MIME);
+      header('Content-Disposition: attachment; filename= aniversariantes.xlsx');
+      header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+      header('Content-Length: ' . filesize('aniversariantes.xlsx'));
+      header('Content-Transfer-Encoding: binary');
+      header('Cache-Control: must-revalidate');
+      header('Pragma: public');
+      header('Expires: 0');
+      finfo_close($finfo);
+      ob_clean();
+      flush();
+      readfile('aniversariantes.xlsx');
+    }
   }
-
-  $objPHPExcel->setActiveSheetIndex(0);
-  $objPHPExcel->getActiveSheet()->setTitle('ANIVERSARIANTES');
- 
-
-  $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-  $objWriter->save('aniversariantes.xlsx');
-  
-  header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-  header('Content-Disposition: attachment;filename="aniversariantes.xlsx"');
-  header('Cache-Control: max-age=0');
-  header('Cache-Control: max-age=1');
-  header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-  header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
-  header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
-  header('Pragma: public'); // HTTP/1.0
-}
 ?>
 <html>
   <head>
